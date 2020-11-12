010edfabafbb805baddba70b8596e4fadb3e01eb
yoshimotokenta <hiraidai10@yahoo.co.jp>
2020-11-12 11:50:19 +0900

    deviseに関するviewの配置

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         validates :nickname, presence: true
+         validates :first_name,presence: true
+         validates :family_name,presence: true
+         validates :first_name_kana,presence: true
+         validates :family_name_kana,presence:true
+         has_many :purchasers
+         has_many :items
 end
