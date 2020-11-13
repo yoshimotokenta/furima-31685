f94298fe40065587b662d896423051b6b3fbeb7a
yoshimotokenta <hiraidai10@yahoo.co.jp>
2020-11-14 08:44:47 +0900

    itemsのモデル及びテーブル作成

---
+++ app/models/item.rb
@@ -0,0 +1,13 @@
+class Item < ApplicationRecord
+  validates :seller_id, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :area_id, presence: true
+  validates :estimated, presence: true
+  validates :name, presence: true
+  validates :info, presence: true
+  validates :price, presence: true
+
+  belongs_to :user
+  has_one :purchaser
+end
