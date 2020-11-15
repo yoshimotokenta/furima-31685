ed0a16138b63f556c412a06d6bb4e1ca34fbf679
yoshimotokenta <hiraidai10@yahoo.co.jp>
2020-11-15 09:21:51 +0900

    ActiveHashを導入,設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,22 @@
 class Item < ApplicationRecord
-  validates :seller_id, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :area_id, presence: true
-  validates :estimated, presence: true
+  validates :seller_id, numericality: { other_than: 1 } 
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :condition_id, numericality: { other_than: 1 } 
+  validates :area_id, numericality: { other_than: 1 } 
+  validates :estimated_id, numericality: { other_than: 1 } 
   validates :name, presence: true
   validates :info, presence: true
   validates :price, presence: true
+  validates :image, presence: true
 
   has_one_attached :image
   belongs_to :user
   has_one :purchaser
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :seller
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :area
+  belongs_to :estimated
 end
